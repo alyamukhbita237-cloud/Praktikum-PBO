870dccaa26c7b96b817e3e45c99eb0addae450e8
alyamukhbita237 <alyamukhbita237@gmail.com>
2026-09-14 21:05:22 +0700

    Percobaan 1 dan 2

---
+++ P3/MotorEncapsulation/MotorDemo.java
@@ -0,0 +1,25 @@
+package P3.MotorEncapsulation;
+
+public class MotorDemo {
+    public static void main(String[] args) {
+        Motor motor = new Motor();
+        motor.printStatus();
+        motor.tambahKecepatan();
+
+        motor.nyalakanMesin();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.matikanMesin();
+        motor.printStatus();
+    }
+
+}
