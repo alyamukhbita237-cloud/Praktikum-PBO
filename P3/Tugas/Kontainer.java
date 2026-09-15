d95a3c6abab6e2f3b5266582f000a7171b1c8b04
alyamukhbita237 <alyamukhbita237@gmail.com>
2026-09-15 15:19:57 +0700

    Tugas Pertanyaan 4

---
+++ P3/Tugas/Kontainer.java
@@ -0,0 +1,52 @@
+package P3.Tugas;
+
+public class Kontainer {
+    private String nomorResi;
+    private String namaPemilik;
+    private double kapasitasMaksimal;
+    private double beratMuatanSaatIni;
+
+    public Kontainer(String nomorResi, String namaPemilik,
+            double kapasitasMaksimal) {
+        this.nomorResi = nomorResi;
+        this.namaPemilik = namaPemilik;
+        this.kapasitasMaksimal = kapasitasMaksimal;
+        this.beratMuatanSaatIni = 0;
+    }
+
+    public String getNomorResi() {
+        return nomorResi;
+    }
+
+    public String getNamaPemilik() {
+        return namaPemilik;
+    }
+
+    public double getKapasitasMaksimal() {
+        return kapasitasMaksimal;
+    }
+
+    public double getBeratMuatanSaatIni() {
+        return beratMuatanSaatIni;
+    }
+
+    public void tambahMuatan(double berat) {
+        if (berat <= 0) {
+            System.out.println("Berat muatan harus lebih dari 0 kg.");
+        } else if (beratMuatanSaatIni + berat > kapasitasMaksimal) {
+            System.out.println("Maaf, berat muatan melebihi kapasitas maksimal kontainer!");
+        } else {
+            beratMuatanSaatIni += berat;
+        }
+    }
+
+    public void turunkanMuatan(double berat) {
+        if (berat <= 0) {
+            System.out.println("Berat yang diturunkan harus lebih dari 0 kg.");
+        } else if (berat > beratMuatanSaatIni) {
+            System.out.println("Maaf, berat yang diturunkan melebihi muatan saat ini! ");
+        } else {
+            beratMuatanSaatIni -= berat;
+        }
+    }
+}
